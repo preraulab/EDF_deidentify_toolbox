500651bf3b4b9969ac8cba53dea6a4d48f9d4788
Thomas Possidente <tpossidente@bwh.harvard.edu>
2021-02-12 09:55:50 -0500

    initial commit

---
+++ matlab/EDF_deidentify.m
@@ -0,0 +1,87 @@
+%EDF_DEIDENTIFY  Deidentify EDF files
+%
+%   Usage:
+%   Direct input:
+%       EDF_deidentify() - Launches gui inputs
+%       EDF_deidentify(filename, copy_file)
+%
+%   Input:
+%       filename: string or cell of strings - filename(s) -- required
+%       copy_file: logical - true: saves a copy of the file with
+%       '-deidentified' tag, false: overwrites file (default: true)
+% 
+%   Copyright 2021 Michael J. Prerau, Ph.D. Laboratory - http://www.sleepEEG.org
+%
+%   Last modified 02/03/2021
+%% ********************************************************************
+
+function EDF_deidentify(filename, copy_file)
+%Sets default to copy file
+if nargin == 1
+    copy_file = true;
+end
+
+%Check to see if any file has been given
+if nargin == 0
+    [filename, path] = uigetfile('*.*',...
+        'Select EDF File(s)', ...
+        'MultiSelect', 'on');
+    answer = questdlg('Do you want to save a copy or overwrite?', ...
+        'Deidentification Menu', ...
+        'Save a Copy', 'Overwrite', 'Cancel', 'Cancel');
+    
+    switch answer
+        case 'Save a Copy'
+            copy_file = true;
+        case 'Overwrite'
+            answer2 = questdlg('Are you sure you want to overwrite? Original information will be lost.', ...
+                'Deidentification Menu', ...
+                'Yes (Overwrite)', 'Cancel', 'Cancel');
+            
+            if strcmpi(answer2,'Yes (Overwrite)')
+                copy_file = false;
+            else
+                return;
+            end
+            
+        otherwise
+            return;
+    end
+    
+else
+    path = ''; %Blank path
+end
+
+%If a single file, wrap in cell to work with the loop
+if ~iscell(filename)
+    filename = {filename};
+end
+
+%Loop through all files and deidentify
+for ii = 1:length(filename)
+    filename_full = fullfile(path, filename{ii});
+    
+    if ~exist(filename_full,'file')
+        error(['Invalid filename: ' filename_full]);
+    end
+    
+    if copy_file
+        filename_new = [filename_full(1:end-4) '_deidentified.edf'];
+        copyfile(filename_full, filename_new);
+        
+        deidentify(filename_new);
+    else
+        deidentify(filename_full);
+    end
+end
+
+function deidentify(filename)
+%Open the file to read/write without resetting it to an empty file
+fid = fopen(filename, 'r+');
+
+fprintf(fid, '%-8s', '0');   % Version must be 0
+fprintf(fid, '%-80s', 'DEIDENTIFIED'); % Remove patient info
+fprintf(fid,'%-80s', 'DEIDENTIFIED'); %Remove recording info
+fprintf(fid, '%02i.%02i.%02i', 1,1,1); % Set date as 01.01.01
+
+fclose(fid);
---
+++ python/EDFdeifentify_function.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+
+
+def edf_deidentify(path, save_dir=None, overwrite=False):
+    """
+
+    :param path: path to edf file to be deidentified
+    :param save_dir: directory to save deidentified copy of edf (default is directory of edf file)
+    :param overwrite: replace the edf file given with the deidentified file (default = False) (Note: if True, ignores
+                      save_dir)
+
+    :return: None
+    """
+
+    # If no save_dir provided, use dir of edf file
+    if save_dir is None:
+        save_dir = os.path.dirname(path)
+    else:  # check if save_dir is valid
+        if not os.path.isdir(save_dir):
+            raise Exception("Invalid save_dir path: " + save_dir)
+
+    # Check if invalid file
+    if not os.path.isfile(path):
+        raise Exception("Invalid file: " + path)
+
+    # Copy file to new name
+    if not overwrite:
+        path_new = save_dir + '/' + os.path.basename(path)[0:-4] + '_deidentified.edf'
+        shutil.copy(path, path_new)
+        path = path_new
+
+    # Open file(s) and deidentify
+    f = open(path, "r+", encoding="iso-8859-1")  # 'r' = read
+    try:
+        f.write('%-8s' % "0")
+        f.write('%-80s' % "DEIDENTIFIED")  # Remove patient info
+        f.write('%-80s' % "DEIDENTIFIED")  # Remove recording info
+        f.write('01.01.01')  # Set date as 01.01.01
+    except UnicodeDecodeError:
+        f.close()
+        f = open(path, "r+", encoding="iso-8859-2")  # 'r' = read
+        try:
+            f.write('%-8s' % "0")
+            f.write('%-80s' % "DEIDENTIFIED")  # Remove patient info
+            f.write('%-80s' % "DEIDENTIFIED")  # Remove recording info
+            f.write('01.01.01')  # Set date as 01.01.01
+        except UnicodeDecodeError:
+            f.close()
+            f = open(path, "r+", encoding="utf-8")  # 'r' = read
+            try:
+                f.write('%-8s' % "0")
+                f.write('%-80s' % "DEIDENTIFIED")  # Remove patient info
+                f.write('%-80s' % "DEIDENTIFIED")  # Remove recording info
+                f.write('01.01.01')  # Set date as 01.01.01
+                f.close()
+            finally:
+                raise Exception('No valid encoding format found')
+
+    return
